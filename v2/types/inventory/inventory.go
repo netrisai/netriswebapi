0521201bca6d49b56a769d03ddf74adea38d7cd7
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2021-08-15 19:03:45 +0400

    feat: inventory: add get by id functionality

--- v2/types/inventory/inventory.go
+++ v2/types/inventory/inventory.go
@@ -18,6 +18,7 @@ package inventory
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/netrisai/netriswebapi/http"
 	v2address "github.com/netrisai/netriswebapi/http/addresses/v2"
@@ -40,6 +41,15 @@ func parseInventories(APIResult *http.APIResponse) ([]*HW, error) {
 	return items, nil
 }
 
+func parseInventory(APIResult *http.APIResponse) (*HW, error) {
+	var items *HW
+	err := http.Decode(APIResult.Data, &items)
+	if err != nil {
+		return items, fmt.Errorf("{parseInventories} %s", err)
+	}
+	return items, nil
+}
+
 func (c *InventoryClient) Get() ([]*HW, error) {
 	address := c.client.URL.String() + v2address.InventoryBase
 	APIResult, err := c.client.Get(address)
@@ -52,4 +62,19 @@ func (c *InventoryClient) Get() ([]*HW, error) {
 		return nil, fmt.Errorf("{Get} %s", err)
 	}
 	return items, nil
+}
+
+func (c *InventoryClient) GetByID(id int) (*HW, error) {
+	address := c.client.URL.String() + v2address.InventoryBase + "/" + strconv.Itoa(id)
+	fmt.Println(address)
+	APIResult, err := c.client.Get(address)
+	if err != nil {
+		return nil, fmt.Errorf("{GetByID} %s", err)
+	}
+
+	hw, err := parseInventory(APIResult)
+	if err != nil {
+		return nil, fmt.Errorf("{GetByID} %s", err)
+	}
+	return hw, nil
 }
