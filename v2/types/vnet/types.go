a20611123c6d9cb6f7653e4e29bfe754e8ea5cee
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2021-08-17 15:39:30 +0400

    fix: vnet: change name field type in VNetUpdateGateway struct

--- v2/types/vnet/types.go
+++ v2/types/vnet/types.go
@@ -231,7 +231,7 @@ type VNetUpdateGuestTenant struct {
 
 type VNetUpdatePort struct {
 	Access bool   `json:"access"`
-	Name   int    `json:"name"`
+	Name   string `json:"name"`
 	ID     int    `json:"id"`
 	Lacp   string `json:"lacp"`
 	State  string `json:"state"`
