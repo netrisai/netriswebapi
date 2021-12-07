339d7ceafd2aedcfad5420690d06aabfef5f84bf
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2021-12-07 16:15:01 +0400

    fix: v2: vnet: breakout field type

--- v2/types/vnet/types.go
+++ v2/types/vnet/types.go
@@ -99,7 +99,7 @@ type VNetDetailedPort struct {
 	Access           bool                             `json:"access"`
 	AdminDown        string                           `json:"adminDown"`
 	AutoNeg          string                           `json:"autoNeg"`
-	Breakout         int                              `json:"breakout"`
+	Breakout         string                           `json:"breakout"`
 	CreatedDate      int                              `json:"createdDate"`
 	Description      string                           `json:"description"`
 	DesiredSpeed     interface{}                      `json:"desiredSpeed"`
