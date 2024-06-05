fc2bcdb8dadb8090519fbc775eccb86801a70af2
Gor Poghosyan <gor@netris.ai>
2024-06-04 17:15:33 -0700

    ignore 304 in get replies

--- http/http.go
+++ http/http.go
@@ -259,7 +259,7 @@ func (cred *HTTPCred) Get(address string) (*APIResponse, error) {
 		return nil, fmt.Errorf("{http.Get} %s", err)
 	}
 
-	if data.StatusCode != http.StatusOK {
+	if data.StatusCode != http.StatusOK && data.StatusCode != http.StatusNotModified {
 		return nil, fmt.Errorf("{http.Get} %s", data.Data)
 	}
 
