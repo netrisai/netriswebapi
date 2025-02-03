c5b263758734aa4066f25f8e77cc806b190c724f
Gor Poghosyan <gor@netris.ai>
2025-02-03 15:11:30 -0800

    added Link update function

--- v2/types/link/link.go
+++ v2/types/link/link.go
@@ -94,7 +94,7 @@ func (c *Client) Add(link *Linkw) (reply http.HTTPReply, err error) {
 	return reply, nil
 }
 
-func (c *Client) Update(id int, link *Linkw) (reply http.HTTPReply, err error) {
+func (c *Client) Update(id int, link *LinkU) (reply http.HTTPReply, err error) {
 	js, err := json.Marshal(link)
 	if err != nil {
 		return http.HTTPReply{}, fmt.Errorf("{UpdateLink} %s", err)
