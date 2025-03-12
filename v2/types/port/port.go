daf9ce118da84ba15867c5b04e08def72742caf5
Gor Poghosyan <gor@netris.ai>
2025-03-12 14:46:42 -0700

    added GetBySwName method in ports

--- v2/types/port/port.go
+++ v2/types/port/port.go
@@ -75,6 +75,20 @@ func (c *PortClient) GetBySites(sites []int) ([]*Port, error) {
 	return items, nil
 }
 
+func (c *PortClient) GetBySwName(swname string) ([]*Port, error) {
+	address := c.client.URL.String() + v2address.Ports + "?filterSwitchName=" + swname
+	APIResult, err := c.client.Get(address)
+	if err != nil {
+		return nil, fmt.Errorf("{GetBySwName} %s", err)
+	}
+
+	items, err := parse(APIResult)
+	if err != nil {
+		return nil, fmt.Errorf("{GetBySwName} %s", err)
+	}
+	return items, nil
+}
+
 func (c *PortClient) GetByID(id int) (*Port, error) {
 	address := c.client.URL.String() + v2address.Ports + "/" + strconv.Itoa(id)
 	APIResult, err := c.client.Get(address)
